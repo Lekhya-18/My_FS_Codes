4a3c696930059100fa9f9189d77afd0c51eccb4a
Lekhya-18 <pittalekhya.24.cse@anits.edu.in>
2026-02-16 21:32:00 +0530

    Updated for backend deployment

--- Form/backend/server.js
+++ Form/backend/server.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
 
 const Life = require("./files/life");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/lifeform")
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
@@ -30,5 +36,5 @@ app.get("/all", async (req, res) => {
   }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
